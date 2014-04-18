ffc2cb31bfe0d80538973b8f6294e59215534e7e
Sebastian Troy <sebastiantroy@btinternet.com>
2014-04-18 21:55:48 +0100

    -Updated .gitignore - First commit, fully working, only need to add some more documentation

---
+++ EveryRGBPictureMatcher/run.bat
@@ -0,0 +1,1 @@
+java -jar "all RGB hillclimber.jar"
---
+++ EveryRGBPictureMatcher/src/Main.java
@@ -0,0 +1,22 @@
+import tCode.TCode;
+
+public class Main extends TCode
+	{
+		public Main(int width, int height, boolean framed, boolean resizable)
+			{
+				super(900, 700, framed, resizable);
+
+				DEBUG = true;
+				FORCE_SINGLE_THREAD = true;
+
+				programName = "All RGB picture approximator";
+				versionNumber = "1.0";
+
+				begin(new PictureApproximator());
+			}
+
+		public static void main(String[] args)
+			{
+				new Main(900, 600, true, true);
+			}
+	}
