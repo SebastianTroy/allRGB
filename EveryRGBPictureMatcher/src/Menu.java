df1e131f88ecb2bda6e042eac4eae41f217ea2d0
Sebastian Troy <sebastiantroy@btinternet.com>
2014-04-20 00:12:00 +0100

    Started implementing an options menu so that the user can select from multiple algorithms

---
+++ EveryRGBPictureMatcher/src/Menu.java
@@ -0,0 +1,28 @@
+import java.awt.Graphics2D;
+
+import tCode.RenderableObject;
+
+public class Menu extends RenderableObject
+	{
+		
+		// TODO create an options menu so that the user can select which algorithm to run
+		protected void initiate()
+			{
+
+			}
+
+		protected void refresh()
+			{
+
+			}
+
+		public void tick(double secondsPassed)
+			{
+
+			}
+
+		protected void render(Graphics2D g)
+			{
+
+			}
+	}
